b38c1ac6e473e78108c07f62aa0117c5e29c5ea9
Wen <wenhuazhen96@gmail.com>
2024-09-14 02:38:58 -0400

    Implemented movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,9 +4,42 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour{
-    private Vector2 movement;
+    private Vector2 moveInput;
+    Rigidbody2D myRigidBody;
+    Animator myAnimator;
+    [SerializeField] float playerSpeed = 5f;
 
-    private void OnMovement(InputValue value) {
-        movement = value.Get<Vector2>();
+    void Start() {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+    }
+
+    void Update() {
+        Run();
+        FlipSprite();
+    }
+
+    private void OnMove(InputValue value) {
+        moveInput = value.Get<Vector2>();
+    }
+
+    private void Run() {
+        //determine if player's velocity is above 0
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+
+        //responsible for controlling player movement velocity
+        Vector2 playerVelocity = new Vector2(moveInput.x * playerSpeed, moveInput.y * playerSpeed);
+        myRigidBody.velocity = playerVelocity;
+
+        //responsible for switching the animation of player from idle to running
+        myAnimator.SetBool("isRunning", playerHasHorizontalSpeed);
+    }
+    private void FlipSprite() {
+        //determine if player's velocity is above 0
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+
+        //if player is moving, change scale to (+/-) to flip the player sprite
+        if(playerHasHorizontalSpeed)
+            transform.localScale = new Vector2 (Mathf.Sign(myRigidBody.velocity.x), 1f);
     }
 }
